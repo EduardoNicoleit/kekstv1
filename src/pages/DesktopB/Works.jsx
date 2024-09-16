d4c19b02ea64394495ceb2e011fcc385d223919e
eduardonicoleit <eduardonicoleit@hotmail.com>
2024-09-16 13:50:40 -0700

    Bg for Our Work Section and title

--- src/pages/DesktopB/Works.jsx
+++ src/pages/DesktopB/Works.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Selection from '../../components/Selection';
 import { selectionsData } from '../../data/selections';
+import Divider from '../../components/Divider';
 
 const Works = () => {
     return (
         <div className="works section-background">
+            <Divider title="Our Work" />
             <Selection selectionsData={selectionsData} />
         </div>
     );
