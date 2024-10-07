07e2256f363271f6b53a92781453b47b11df7389
eduardonicoleit <eduardonicoleit@hotmail.com>
2024-10-07 18:00:46 -0300

    Fixing Sticky navbar

--- src/App.jsx
+++ src/App.jsx
@@ -20,36 +20,34 @@ import {
   Uniper,
   Volkswagen
 } from './pages';
-import './App.css'
-import useSticky from './hooks/useSticky';
+import './App.css';
 
 function App() {
-  const { isSticky, element } = useSticky()
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home isSticky={isSticky} element={element} />} />
-        <Route index path="/home" element={<Home isSticky={isSticky} element={element} />} />
-        <Route path="/expertise" element={<Expertise isSticky={isSticky} element={element} />} />
-        <Route path="/product" element={<Product isSticky={isSticky} element={element} />} />
-        <Route path="/expertise/datas" element={<ExpertiseData isSticky={isSticky} element={element} />} />
-        <Route path="/works" element={<Works isSticky={isSticky} element={element} />} />
-        <Route path="/works/bp" element={<Bp isSticky={isSticky} element={element} />} />
-        <Route path="/works/bpl" element={<Bpl isSticky={isSticky} element={element} />} />
-        <Route path="/works/ceoTracker" element={<CeoTracker isSticky={isSticky} element={element} />} />
-        <Route path="/works/copReport" element={<CopReport isSticky={isSticky} element={element} />} />
-        <Route path="/works/deutsche" element={<Deutsche isSticky={isSticky} element={element} />} />
-        <Route path="/works/dtcp" element={<Dtcp isSticky={isSticky} element={element} />} />
-        <Route path="/works/farmerVoice" element={<FarmerVoice isSticky={isSticky} element={element} />} />
-        <Route path="/works/maltesert" element={<Malteser isSticky={isSticky} element={element} />} />
-        <Route path="/works/mars" element={<Mars isSticky={isSticky} element={element} />} />
-        <Route path="/works/netClean" element={<NetClean isSticky={isSticky} element={element} />} />
-        <Route path="/works/newwaters" element={<Newwaters isSticky={isSticky} element={element} />} />
-        <Route path="/works/uniper" element={<Uniper isSticky={isSticky} element={element} />} />
-        <Route path="/works/volkswagen" element={<Volkswagen isSticky={isSticky} element={element} />} />
+        <Route path="/" element={<Home />} />
+        <Route index path="/home" element={<Home />} />
+        <Route path="/expertise" element={<Expertise />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/expertise/datas" element={<ExpertiseData />} />
+        <Route path="/works" element={<Works />} />
+        <Route path="/works/bp" element={<Bp />} />
+        <Route path="/works/bpl" element={<Bpl />} />
+        <Route path="/works/ceoTracker" element={<CeoTracker />} />
+        <Route path="/works/copReport" element={<CopReport />} />
+        <Route path="/works/deutsche" element={<Deutsche />} />
+        <Route path="/works/dtcp" element={<Dtcp />} />
+        <Route path="/works/farmerVoice" element={<FarmerVoice />} />
+        <Route path="/works/maltesert" element={<Malteser />} />
+        <Route path="/works/mars" element={<Mars />} />
+        <Route path="/works/netClean" element={<NetClean />} />
+        <Route path="/works/newwaters" element={<Newwaters />} />
+        <Route path="/works/uniper" element={<Uniper />} />
+        <Route path="/works/volkswagen" element={<Volkswagen />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,41 +1,56 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import Logo from '../assets/img/navbar/logo_02.png';
-
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import "./Navbar.css";
 
-const Navbar = ({ sticky }) => {
+const Navbar = () => {
+    const [isSticky, setSticky] = useState(false);
+    const navbarRef = useRef(null);
+
+    const handleScroll = useCallback(() => {
+        if (navbarRef.current) {
+            setSticky(window.scrollY > navbarRef.current.offsetTop);
+        }
+    }, []);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [handleScroll]);
+
     const [toggle, setToggle] = useState(false);
     const menuRef = useRef(null);
 
-    const handleToggle = (event) => {
+    const handleToggle = useCallback((event) => {
         event.stopPropagation();
-        setToggle(!toggle);
-    };
+        setToggle((prev) => !prev);
+    }, []);
 
-    const handleClickOutside = (event) => {
+    const handleClickOutside = useCallback((event) => {
         if (menuRef.current && !menuRef.current.contains(event.target)) {
             setToggle(false);
         }
-    };
+    }, []);
 
     useEffect(() => {
         document.addEventListener('mousedown', handleClickOutside);
         return () => {
             document.removeEventListener('mousedown', handleClickOutside);
         };
-    }, []);
+    }, [handleClickOutside]);
 
     return (
         <div
-            className={sticky ? "navbar navbar-sticky h-[100PX] z-50" : "navbar h-[100px] xl:mt-[25px] 2xl:mt-[45px] 3xl:mt-[50px] z-50"}>
+            ref={navbarRef}
+            className={`${isSticky ? "navbar navbar-sticky" : "navbar mt-0 md:mt-4 lg:mt-14"} h-[100px]`}
+        >
             <div className='w-full'>
                 <div className='pl-[17px] md:pl-4 lg:pl-12 2xl:pl-[128px] border-white h-16 w-full flex items-center justify-between'>
                     <Link to="/">
-                        <img src={Logo} alt="logo" className='cursor-pointer 
-                        w-auto
-                        h-auto 2xl:w-auto'/>
+                        <img src={Logo} alt="logo" className='cursor-pointer w-auto h-auto 2xl:w-auto' />
                     </Link>
                     <div className='flex w-full 2xl:pl-[50px] 3xl:pl-[70px] justify-between'>
                         <div className="xl:pl-[50px] lg:flex h-[50px] py-[13px] font-Arial font-normal text-white">
@@ -50,31 +65,28 @@ const Navbar = ({ sticky }) => {
                         </div>
                         <motion.div
                             whileTap={{ scale: 0.6 }}
-                            className="block 2xl:hidden cursor-pointer items-center h-full my-auto" // Center vertically
+                            className="block 2xl:hidden cursor-pointer items-center h-full my-auto"
                             onClick={handleToggle}
                         >
-                            {
-                                !toggle ? (
-                                    <div className='text-white uppercase font-Helvetica font-normal text-[11px] md:text-xs lg:text-[14px] xl:text-[22px] md:py-[7px] lg:py-[10px] xl:py-4 2xl:py-4 md:px-[11px] lg:px-[18px] xl:px-7 2xl:px-7 border-white border-0 md:border-[1px] mr-[17px] md:mr-4 lg:mr-12 2xl:mr-[128px]'>
-                                        Menu
-                                    </div>
-                                ) : (
-                                    <div className='font-Helvetica font-normal text-base lg:text-lg xl:text-[22px] text-white pr-6 sm:pr-4 md:pr-8 lg:pr-16 xl:pr-20'>
-                                        Close
-                                    </div>
-                                )
-                            }
+                            {toggle ? (
+                                <div className='font-Helvetica font-normal text-base lg:text-lg xl:text-[22px] text-white pr-6'>
+                                    Close
+                                </div>
+                            ) : (
+                                <div className='text-white uppercase font-Helvetica font-normal text-[11px] md:text-xs lg:text-[14px] xl:text-[22px] py-4 md:px-[11px] lg:px-[18px] xl:px-7'>
+                                    Menu
+                                </div>
+                            )}
                         </motion.div>
                     </div>
                 </div>
             </div>
-            {/* Mobile menu */}
             {toggle && (
                 <div ref={menuRef} className="2xl:hidden fixed top-16 left-0 w-full bg-[#210837] bg-opacity-90 text-white z-50">
                     <ul className='flex flex-col items-start py-4 px-4 sm:px-6 md:px-8 text-sm sm:text-base md:text-lg'>
                         <li className="py-2 w-full hover:text-gray-400"><Link to="/" onClick={handleToggle}>Expertise</Link></li>
                         <li className="py-2 w-full hover:text-gray-400"><Link to="/" onClick={handleToggle}>Our work</Link></li>
-                        <li className="py-2 w-full hover:text-gray-400"><Link to="/" onClick={handleToggle}>Get to know us.</Link></li>
+                        <li className="py-2 w-full hover:text-gray-400"><Link to="/" onClick={handleToggle}>Get to know us</Link></li>
                         <li className="py-2 w-full hover:text-gray-400"><Link to="/" onClick={handleToggle}>Our design thinking process</Link></li>
                         <li className="py-2 w-full hover:text-gray-400"><Link to="/" onClick={handleToggle}>Our Manifesto</Link></li>
                         <li className="py-2 w-full hover:text-gray-400"><Link to="/" onClick={handleToggle}>Let's talk</Link></li>
