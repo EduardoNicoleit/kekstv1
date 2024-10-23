04dc5fb6824428ea402d0ad2cd46a8a482f0e457
eduardonicoleit <eduardonicoleit@hotmail.com>
2024-10-23 16:13:10 -0300

    Works external page

--- src/components/WorkDataSelection.jsx
+++ src/components/WorkDataSelection.jsx
@@ -6,24 +6,23 @@ const Selections = ({ selectionsData }) => {
         <div className="overflow-hidden">
             {selectionsData && selectionsData.map((data, index) => (
                 <div key={data.id} className={`flex flex-col md:flex-row text-white sm:px-0 md:px-0  ${data.id !== 1 ? ' pt-[50px] lg:pt-[60px] xl:pt-[72px] 2xl:pt-0' : ' pt-[50px] lg:pt-0'}`}>
-                    <div className='w-0 lg:w-[34px] xl:w-[65px] 2xl:w-[306px]'>
+                    <div className='w-0 lg:w-[34px] xl:w-[65px] 2xl:w-[308px]'>
                     </div>
-                    <div className='flex-1 m-0 p-0 border-white border-b-[1px] lg:border-b-0'>
+                    <div className='flex-1 m-0 p-0 border-white border-b-[1px] 3xl:border-b-[2px] lg:border-b-0'>
                         <Link to={data.link_url} >
-                            <div className="relative overflow-hidden w-full">
-                                <img src={data.img_url} alt="logo" className="hidden sm:block pt-8 h-auto w-full cursor-pointer transition duration-300 ease-in-out hover:scale-105 object-cover" />
+                            <div className="relative overflow-hidden w-full 2xl:h-[24vw]">
+                                <img src={data.img_url} alt="logo" className="hidden sm:block pt-8 2xl:pt-[2vw] h-auto w-full cursor-pointer transition duration-300 ease-in-out hover:scale-105 object-cover" />
                                 <img src={data.mobile_img_url} alt="logo" className="block sm:hidden h-auto w-full cursor-pointer transition duration-300 ease-in-out hover:scale-105 object-cover" />
                             </div>
                         </Link>
-                        <div className='flex flex-col lg:flex-row font-bold items-center px-[33px] lg:px-0
-                        py-0 sm:py-5 md:py-8 xl:py-[26px] 2xl:py-8 mt-8 md:mt-0 mb-12 md:mb-0'>
-                            <div className='w-full flex lg:w-[280px] xl:w-[270px] flex-none 2xl:w-[514px] items-center sm:border-none pb-2 xl:pb-0'>
-                                <div className='md:text-[21px] lg:text-[30px] xl:text-4xl font-bold font-Helvetica'>
+                        <div className='flex flex-col lg:flex-row font-bold items-center px-[33px] lg:px-0 py-0 sm:py-5 md:py-8 xl:py-[26px] 2xl:py-[2.5vw] mt-8 md:mt-0 mb-12 md:mb-0'>
+                            <div className='w-full flex lg:w-[280px] xl:w-[270px] flex-none 2xl:w-[20vw] items-center sm:border-none pb-2 xl:pb-0'>
+                                <div className='md:text-[21px] lg:text-[30px] xl:text-4xl 2xl:text-[2vw] font-bold font-Helvetica 2xl:pr-[1vw] 3xl:leading-[2.5vw]'>
                                     {data.title}
                                 </div>
-                                <div className='hidden 2xl:block h-[1px] bg-white ml-5 w-full'></div>
+                                <div className='hidden 2xl:block h-[1px] 3xl:h-[2px] bg-white ml-5 w-full 2xl:ml-[1vw]'></div>
                             </div>
-                            <div className='w-full font-normal text-[17px] lg:text-[18px] border-0 border-r-0 2xl:border-l-[1px] pl-0 lg:pl-8 font-Helvetica py-2 lg:py-0'>
+                            <div className='w-full font-normal text-[17px] lg:text-[18px] 2xl:text-[1.4vw] 5xl:text-[1.1vw] border-0 border-r-0 2xl:border-l-[1px] 3xl:border-l-[2px] pl-0 lg:pl-8 2xl:pl-[4vw] 2xl:pr-[4vw] font-Helvetica py-2 lg:py-0 2xl:py-[1vw]'>
                                 {data.content}
                             </div>
                         </div>
--- src/pages/WorksData.jsx
+++ src/pages/WorksData.jsx
@@ -38,35 +38,26 @@ const WorksDataCompo = ({ isSticky, element }) => {
             </div>
             <div className="w-full text-white px-2 lg:px-4 xl:px-[25px] 2xl:px-[45px]">
                 <div className="border-b-0 lg:border-b-[1px]">
-                    <div className="font-Helvetica font-bold 
-                    text-[32px] sm:text-[45px] lg:text-[68px] xl:text-[90px] 2xl:text-[120px]
-                    pt-8 sm:pt-8 lg:pt-[88px] xl:pt-[134px] 2xl:pt-[170px]
-                    pb-10 sm:pb-8 lg:pb-[67px] xl:pb-9 2xl:pb-[50px]
-                    pl-[33px] lg:pl-[41px] xl:pl-[65px] 2xl:pl-[306px]
-                    ">
+                    <div className="font-Helvetica font-bold text-[32px] sm:text-[45px] lg:text-[68px] xl:text-[90px] 2xl:text-[120px] 3xl:text-[5.5vw] pt-8 sm:pt-8 lg:pt-[88px]xl:pt-[134px] 2xl:pt-[170px] pb-10 sm:pb-8 lg:pb-[67px] xl:pb-9 2xl:pb-[50px] pl-[33px] lg:pl-[41px] xl:pl-[65px] 2xl:pl-[308px]">
                         A selection of work
                     </div>
                 </div>
             </div>
             <div className="">
                 <div className="px-2 lg:px-0">
-                    <div className="text-white border-b-[1px] md:border-b-0 px-[33px] lg:px-[49px] xl:px-[90px] 2xl:px-[356px]">
-                        <p className="font-Helvetica font-bold sm:font-normal
-                    text-[17px] lg:text-[30px] 2xl:text-[37px]
-                    py-0 lg:py-[78px] 2xl:py-[74px]
-                    pb-[58px] md:pb-0
-                    leading-tight
-                    ">
+                    <div className="text-white border-b-[1px] md:border-b-0 px-[33px] lg:px-[49px] xl:px-[90px] 2xl:px-[356px] 3xl:pr-[7vw]">
+                        <p className="font-Helvetica font-bold sm:font-normal text-[17px] lg:text-[30px] 2xl:text-[37px] 3xl:text-[2vw] py-0 lg:py-[78px] 2xl:py-[4vw] pb-[58px] md:pb-0 2xl:pb-[10vw] leading-tight">
                             Unlocking new narratives in a digital age. The digital landscape is vast, constantly evolving, and ever-challenging. But with challenges come opportunities. Through Strategic Visual Communications, Product & Platform Creation, and Data Visualization, we empower brands to seize these opportunities. With a blend of creativity, technology, and strategy, we're not just designing for the present but envisioning the future.
                         </p>
                     </div>
                 </div>
             </div>
             <div className="pt-0 2xl:pt-12 px-2 lg:px-4 xl:px-[25px] 2xl:px-[45px]">
-                <div className="border-b-[1px] border-white">
+                <div className="border-b-[1px] border-white 3xl:pb-[5.5vw]">
                     <WorkDataSelection selectionsData={selectionsData} />
                 </div>
             </div>
+            <pr />
             <Contact />
             <MainBorder />
         </div>
