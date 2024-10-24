d516321ac1a76ecff4b92ee830fb0c66853d1685
eduardonicoleit <eduardonicoleit@hotmail.com>
2024-10-24 10:49:05 -0300

    NetClean

--- src/pages/NetClean.jsx
+++ src/pages/NetClean.jsx
@@ -26,17 +26,12 @@ const Netclean = ({ isSticky, element }) => {
                     className="w-full block md:hidden"
                 />
                 <div className="w-full absolute bottom-0 font-Helvetica">
-                    <div className="flex flex-col px-2 lg:px-4 xl:px-[25px] 2xl:px-[45px] 3xl:px-[45px]">
+                    <div className="flex flex-col px-2 lg:px-4 xl:px-[25px] 2xl:px-[45px]">
                         <div className="flex">
-                            <div className="w-[33px] lg:w-[60px] 2xl:w-[299px] flex-none"></div>
+                            <div className="w-[33px] lg:w-[60px] 2xl:w-[299px] 3xl:w-[15vw] flex-none"></div>
                             <div>
                                 <div
-                                    className="font-Helvetica font-bold mt-auto
-                                    text-[30px] leading-[34px] md:text-[50px] md:leading-[57px]
-                                    lg:text-[70px]
-                                    xl:text-[160px]
-                                    lg:leading-[80px] xl:leading-[184px]
-                                    py-14 sm:py-6 md:py-10 lg:py-10 xl:py-5 2xl:py-16 3xl:py-20"
+                                    className="font-helvetica font-bold mt-auto text-[50px] leading-[34px] md:text-[50px] md:leading-[57px] lg:text-[70px] xl:text-[160px] 2xl:text-[5.5vw] 3xl:text-[6.5vw] lg:leading-[80px] xl:leading-[184px] py-14 sm:py-6 md:py-10 lg:py-10 xl:py-5 2xl:py-16 3xl:py-[6vw]"
                                 >
                                     Netclean
                                 </div>
@@ -45,22 +40,31 @@ const Netclean = ({ isSticky, element }) => {
                         <div className="hidden lg:block">
                             <div
                                 className="lg:flex-col 2xl:flex-row flex border-white border-y-[1px]
-                                sm:text-4xl md:text-3xl lg:text-[22px] lg:h-[220px] xl:h-[188px] 2xl:h-[128px] items-start 2xl:items-center"
+                                sm:text-4xl md:text-3xl lg:text-[22px] 3xl:text-[1.2vw] 5xl:text-[1vw]
+                                lg:h-[220px] xl:h-[188px] 2xl:h-[128px] 3xl:h-[15vh] 5xl:h-[18vh]
+                                items-start 2xl:items-center"
                             >
                                 <div
-                                    className="lg:pl-[60px] lg:py-4 xl:py-5 2xl:py-0
-                                    w-[300px] lg:w-[200px] 2xl:w-[318px] mb-0 2xl:mb-12"
+                                    className="text-normal lg:pl-[60px] lg:pr-0 lg:py-4 xl:py-5 2xl:py-[2vw] 
+                                    w-[300px] lg:w-[200px] 2xl:w-[318px] 3xl:w-[14vw] 
+                                    mb-0 2xl:mb-12 
+                                    3xl:pl-[2vw] 3xl:pr-[2vw] 3xl:py-[2vw] 5xl:pl-[3vw] 5xl:pr-[3vw] 5xl:py-[3vw]"
                                 >
-                                    Overview
+                                    Overview:
                                 </div>
-                                <div className="lg:grid grid-cols-3 w-full items-start 2xl:items-center">
-                                    <div className="pl-[60px] 2xl:pl-8 pr-4 xl:border-l-0 2xl:border-l-[1px] leading-tight h-full">
+                                <div className="text-normal lg:grid grid-cols-3 w-full items-start 2xl:items-center 
+                                                3xl:gap-[1vw] 5xl:gap-[1.5vw]">
+                                    <div className="pl-[60px] 2xl:pl-8 pr-4 xl:border-l-0 2xl:border-l-[1px] 
+                                                leading-tight h-full 
+                                                3xl:pl-[2.15vw] 3xl:pr-[1vw] 5xl:pl-[2.5vw] 5xl:pr-[1.2vw]">
                                         Reinvented NetClean's Annual Reports strategy.
                                     </div>
-                                    <div className="pl-8 pr-4 border-l-[1px] leading-tight h-full">
+                                    <div className="pl-8 pr-4 border-l-[1px] leading-tight h-full 
+                                                3xl:pl-[2vw] 3xl:pr-[2vw] 5xl:pl-[3vw] 5xl:pr-[2vw]">
                                         Crafted a multi-channel, visual approach to communicate results and core values.
                                     </div>
-                                    <div className="pl-8 pr-4 border-l-[1px] leading-tight h-full">
+                                    <div className="pl-8 pr-4 border-l-[1px] leading-tight h-full 
+                                                3xl:pl-[2vw] 2xl:pr-[3vw] 5xl:pl-[3vw] 5xl:pr-[2vw]">
                                         Converted data into dynamic stories for stakeholder engagement.
                                     </div>
                                 </div>
@@ -69,7 +73,7 @@ const Netclean = ({ isSticky, element }) => {
                     </div>
                 </div>
             </div>
-            <div className="px-2 lg:px-4 xl:px-[25px] 2xl:px-[45px] 3xl:px-[45px]">
+            <div className="px-2 lg:px-4 xl:px-[25px] 2xl:px-[45px]">
                 {/* Mobile Overview */}
                 <div className="block lg:hidden font-Helvetica font-normal py-[41px] text-white pl-6 sm:pl-[33px]">
                     <div className="text-[12px] leading-[14px] uppercase">Overview</div>
@@ -85,21 +89,26 @@ const Netclean = ({ isSticky, element }) => {
                 </div>
                 {/* Partnership Section */}
                 <div
-                    className="flex flex-col 2xl:flex-row md:pt-[10px] lg:pt-10 xl:pt-[156px] 2xl:pt-[211px] pb-10 lg:pb-20 xl:pb-[106px] 2xl:pb-[134px] w-full font-Helvetica text-white"
+                    className="flex flex-col 2xl:flex-row md:pt-[10px] lg:pt-10 xl:pt-[156px] 2xl:pt-[211px] 
+                    pb-10 lg:pb-20 xl:pb-[106px] 2xl:pb-[134px] w-full font-Helvetica text-white"
                 >
                     <div
-                        className="text-start 2xl:text-center font-normal
-                        text-[12px] md:text-lg 2xl:text-2xl
-                        pt-3
-                        xl:w-full 2xl:w-[299px]
-                        flex-none
-                        px-[33px] lg:px-[60px] 2xl:px-0
-                        py-6 2xl:py-0"
+                        className="uppercase xl:normal-case text-start 2xl:text-left font-normal 
+                        text-[12px] md:text-lg 2xl:text-2xl 3xl:text-[1.2vw] 5xl:text-[1vw] 
+                        pt-3 xl:w-full 2xl:w-[299px] 3xl:w-[15vw] 
+                        flex-none 
+                        px-6 lg:px-[60px] 2xl:px-[2vw] 
+                        py-6 2xl:py-0 
+                        3xl:px-[2vw] 5xl:px-[3vw] 
+                        3xl:py-[2vw] 5xl:py-[3vw]"
                     >
                         The partnership:
                     </div>
                     <div
-                        className="font-bold md:font-normal text-[17px] md:text-[28px] 2xl:text-[40px] px-[33px] lg:px-[60px] 2xl:px-0 w-full leading-tight"
+                        className="font-bold md:font-normal text-[17px] md:text-[28px] 2xl:text-[40px] 
+                        3xl:text-[2vw] 5xl:text-[2.5vw] 
+                        px-6 lg:px-[60px] 2xl:px-0 2xl:pr-[3.5vw] 
+                        w-full leading-tight"
                     >
                         In our partnership with NetClean, we revolutionized the concept of Annual Reports. We initiated a narrative strategy that resonated with the company's modern and bold ethos, marking a fundamental shift in how NetClean communicates its core values and achievements.
                     </div>
@@ -118,10 +127,13 @@ const Netclean = ({ isSticky, element }) => {
                 </div>
                 {/* Description Section */}
                 <div
-                    className="py-10 2xl:py-[114px] text-[17px] lg:text-[18px] xl:text-2xl flex font-normal font-Helvetica text-white"
+                    className="py-10 2xl:py-[5vw] text-[17px] lg:text-[18px] xl:text-[1.2vw] 
+                    flex font-normal font-Helvetica text-white 
+                    3xl:text-[1.5vw] 5xl:text-[2vw] 
+                    3xl:py-[6vw] 5xl:py-[8vw]"
                 >
-                    <div className="md:w-[33px] lg:w-[60px] 2xl:w-[299px] flex-none"></div>
-                    <div className="px-6 md:px-0">
+                    <div className="w-6 lg:w-[60px] 2xl:w-[299px] 3xl:w-[15vw] flex-none"></div>
+                    <div className="px-0 pr-6 sm:px-6 lg:px-0 lg:pr-10 2xl:pr-[3.5vw]">
                         <div className="pb-5">
                             Our strategy wove this narrative across various channels, creating a dialogue with stakeholders that went beyond the traditional confines of reporting. The aim was to transform static data into a compelling visual narrative, utilizing infographics, illustrations, and animations not just to capture attention, but to make complex information engaging and relatable.
                         </div>
@@ -153,7 +165,7 @@ const Netclean = ({ isSticky, element }) => {
                     <div className="hidden lg:block lg:w-[299px] flex-none border-b-[1px]"></div>
                     <div className="w-full flex">
                         <video
-                            className="w-1/2 h-auto object-cover"
+                            className="w-1/2 h-auto object-cover 3xl:max-h-[25vw] 5xl:max-h-[30vw]"
                             autoPlay
                             loop
                             muted
@@ -162,7 +174,7 @@ const Netclean = ({ isSticky, element }) => {
                             <source src="/assets/video/netclean/NetClean_movie_2.mp4" type="video/mp4" />
                         </video>
                         <video
-                            className="w-1/2 h-auto object-cover"
+                            className="w-1/2 h-auto object-cover 3xl:max-h-[25vw] 5xl:max-h-[30vw]"
                             autoPlay
                             loop
                             muted
@@ -184,33 +196,46 @@ const Netclean = ({ isSticky, element }) => {
                     </div>
                 </div>
                 {/* Two Images with Hover Effect */}
-                <div className="w-full border-t border-b border-white">
+                <div className="w-full border-t border-b border-white pt-0">
                     <div className="flex text-white font-Helvetica">
-                        <div className="grid grid-cols-2 w-full">
+                        <div className="grid grid-cols-2 w-full 3xl:gap-[1vw] 5xl:gap-[1.5vw]">
                             {/* First Image Container */}
                             <div className="overflow-hidden md:border-r md:border-white">
                                 <Link to="/works/volkswagen">
                                     <div
-                                        className="cursor-pointer py-[26px] 2xl:py-[100px] px-[15px] md:px-10 flex flex-col h-full
-                                        bg-[url('assets/img/netclean/vw_02.png')] bg-no-repeat bg-cover
-                                        transition duration-300 ease-in-out hover:scale-105"
+                                        className="cursor-pointer py-[26px] 2xl:py-[100px] 3xl:py-[6vw] 
+                                        px-[15px] md:px-10 3xl:px-[2vw] flex flex-col h-full 
+                                        bg-[url('assets/img/netclean/vw_02.png')] bg-no-repeat bg-cover 
+                                        transition duration-300 ease-in-out hover:scale-105 
+                                        5xl:py-[10vw] 5xl:px-[3vw]"
                                     >
-                                        <div className="font-bold text-[15px] lg:text-base xl:text-2xl">Volkswagen</div>
+                                        <div className="font-bold text-[15px] lg:text-base xl:text-2xl 
+                                                        3xl:text-[1.5vw] 5xl:text-[1vw]">
+                                            Volkswagen
+                                        </div>
                                         <div className="flex-1">
                                             <div
-                                                className="max-w-[440px] py-[26px] lg:py-[43px] xl:py-[67px] font-normal
-                                                text-[12px] lg:text-[21px] xl:text-[32px] 2xl:text-[40px] leading-tight"
+                                                className="max-w-[440px] 3xl:max-w-[20vw] py-[26px] lg:py-[43px] 
+                                                xl:py-[67px] 3xl:py-[4vw] font-normal text-[12px] 
+                                                lg:text-[21px] xl:text-[32px] 2xl:text-[40px] 
+                                                3xl:text-[1.3vw] 5xl:text-[1.2vw] leading-tight 
+                                                5xl:py-[5vw]"
                                             >
-                                                Partnering with Netclean to enhance their global ESG narrative, from strategic audience engagement to shaping future sustainability pathways.
+                                                Partnering with BPI to spotlight its crucial role in advancing the pharmaceutical industry through engaging narratives and innovative visual strategies.
                                             </div>
                                         </div>
-                                        <div className="flex py-8 items-center pl-4">
-                                            <div className="uppercase font-normal text-[13px] lg:text-xs xl:text-lg">
+                                        {/* "Read More" Link */}
+                                        <div className="flex py-8 items-center pl-4 
+                                                        3xl:py-[2vw] 5xl:py-[2.5vw]">
+                                            <div className="uppercase font-normal text-[13px] lg:text-xs xl:text-lg 
+                                                            3xl:text-[1.1vw] 5xl:text-[1.2vw]">
                                                 read more
                                             </div>
                                             <div>
                                                 <svg
-                                                    className="ml-3 pt-[2px] w-6 h-6"
+                                                    className="ml-3 pt-[2px] w-6 h-6 
+                                                               3xl:w-[1.5vw] 3xl:h-[1.5vw] 
+                                                               5xl:w-[1vw] 5xl:h-[1vw]"
                                                     viewBox="0 0 24 24"
                                                     fill="none"
                                                     xmlns="http://www.w3.org/2000/svg"
@@ -229,29 +254,44 @@ const Netclean = ({ isSticky, element }) => {
                                 </Link>
                             </div>
                             {/* Second Image Container */}
-                            <div className="overflow-hidden">
+                            <div className="overflow-hidden border-l border-white">
                                 <Link to="/works/mars">
                                     <div
-                                        className="cursor-pointer py-[26px] 2xl:py-[100px] px-[15px] md:px-10 flex flex-col h-full
-                                        bg-[url('assets/img/netclean/mars_2.png')] md:bg-[url('assets/img/netclean/mars_1.png')]
-                                        bg-no-repeat bg-cover transition duration-300 ease-in-out hover:scale-105"
+                                        className="cursor-pointer py-[26px] 2xl:py-[100px] 3xl:py-[6vw] 
+                                        px-[15px] md:px-10 3xl:px-[2vw] flex flex-col h-full 
+                                        bg-[url('assets/img/netclean/mars_2.png')] 
+                                        md:bg-[url('assets/img/netclean/mars_1.png')] 
+                                        bg-no-repeat bg-cover 
+                                        transition duration-300 ease-in-out hover:scale-105 
+                                        5xl:py-[10vw] 5xl:px-[3vw]"
                                     >
-                                        <div className="font-bold text-[15px] lg:text-base xl:text-2xl">Mars</div>
+                                        <div className="font-bold text-[15px] lg:text-base xl:text-2xl 
+                                                        3xl:text-[1.5vw] 5xl:text-[1vw]">
+                                            Mars
+                                        </div>
                                         <div className="flex-1">
                                             <div
-                                                className="max-w-[440px] py-[26px] lg:py-[43px] xl:py-[67px] font-normal
-                                                text-[12px] lg:text-[21px] xl:text-[32px] 2xl:text-[40px] leading-tight"
+                                                className="max-w-[440px] 3xl:max-w-[20vw] py-[26px] lg:py-[43px] 
+                                                xl:py-[67px] 3xl:py-[4vw] font-normal text-[12px] 
+                                                lg:text-[21px] xl:text-[32px] 2xl:text-[40px] 
+                                                3xl:text-[1.3vw] 5xl:text-[1.2vw] leading-tight 
+                                                5xl:py-[5vw]"
                                             >
                                                 Crafting a video campaign that showcases how artificial intelligence tools are revolutionizing research methodologies within Mars, positioning them at the forefront of innovative exploration.
                                             </div>
                                         </div>
-                                        <div className="flex py-8 items-center pl-4">
-                                            <div className="uppercase font-normal text-[13px] lg:text-xs xl:text-lg">
+                                        {/* "Read More" Link */}
+                                        <div className="flex py-8 items-center pl-4 
+                                                        3xl:py-[2vw] 5xl:py-[2.5vw]">
+                                            <div className="uppercase font-normal text-[13px] lg:text-xs xl:text-lg 
+                                                            3xl:text-[1.1vw] 5xl:text-[1.2vw]">
                                                 read more
                                             </div>
                                             <div>
                                                 <svg
-                                                    className="ml-3 pt-[2px] w-6 h-6"
+                                                    className="ml-3 pt-[2px] w-6 h-6 
+                                                               3xl:w-[1.5vw] 3xl:h-[1.5vw] 
+                                                               5xl:w-[1vw] 5xl:h-[1vw]"
                                                     viewBox="0 0 24 24"
                                                     fill="none"
                                                     xmlns="http://www.w3.org/2000/svg"
