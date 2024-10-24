f1cec378f4e645e0839b949739a522919d87fd11
eduardonicoleit <eduardonicoleit@hotmail.com>
2024-10-24 10:50:57 -0300

    gap removed

--- src/pages/NetClean.jsx
+++ src/pages/NetClean.jsx
@@ -198,7 +198,7 @@ const Netclean = ({ isSticky, element }) => {
                 {/* Two Images with Hover Effect */}
                 <div className="w-full border-t border-b border-white pt-0">
                     <div className="flex text-white font-Helvetica">
-                        <div className="grid grid-cols-2 w-full 3xl:gap-[1vw] 5xl:gap-[1.5vw]">
+                        <div className="grid grid-cols-2 w-full">
                             {/* First Image Container */}
                             <div className="overflow-hidden md:border-r md:border-white">
                                 <Link to="/works/volkswagen">
