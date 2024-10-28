975a2907a0a7d468e5a3f5a1a7b7f84a356ea510
eduardonicoleit <eduardonicoleit@hotmail.com>
2024-10-28 18:35:56 -0300

    Interactive map

--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -137,8 +137,16 @@ Arlington, VA 22201`,
         console.log("Starting InteractiveMap component");
 
         const calculateFontSize = () => {
-            const baseFontSize = 20;
-            const scaleFactor = window.innerWidth / 1920;
+            let baseFontSize;
+            const screenWidth = window.innerWidth;
+
+            if (screenWidth < 768) { // Mobile breakpoint
+                baseFontSize = 32; // Adjust this value as needed
+            } else {
+                baseFontSize = 20;
+            }
+
+            const scaleFactor = screenWidth / 1920;
             return Math.max(baseFontSize * scaleFactor, 8);
         };
 
@@ -390,110 +398,112 @@ Arlington, VA 22201`,
             : selectedCity;
 
     return (
-        <div className="interactive-map flex relative overflow-visible ml-5 pr-4 md:ml-[34px] lg:ml-[59px] xl:ml-[100px] 2xl:ml-[308px] h-[400px] md:h-[500px] lg:h-[500px] xl:h-[600px] 2xl:h-[600px] 3xl:h-[40vw] w-auto border-t border-white">
-            {/* Left Section - "Let's Talk" */}
-            <div className="w-full md:w-1/3 flex-shrink-0 overflow-visible z-10">
-                {/* Left Section Content */}
-                <div className="flex items-center">
-                    <div
-                        id="lets-talk"
-                        className="font-Helvetica font-bold text-[32px] md:text-[32px] lg:text-[55px] xl:text-[65px] 2xl:text-[4vw] leading-tight pt-2 md:pt-4"
-                    >
-                        Let’s talk
-                    </div>
-                </div>
-                <div className="sm:pl-0 w-[200px] sm:w-[300px] xl:w-[350px] 2xl:w-[400px] 3xl:w-[25vw]">
-                    <img src={lineImg} alt="Divider Line" className="w-full h-auto" />
-                </div>
-                <a href="mailto:creative@kekstcnc.com">
-                    <div className="sm:pl-0 flex items-center gap-2 pt-6 sm:pt-3 lg:pt-6 xl:pt-10 2xl:pt-[57px] 3xl:pt-[3vw] hover:text-[#7a6b87]">
-                        <div className="flex items-center justify-center">
-                            <svg
-                                className="w-[21px] h-[16px] 2xl:w-[25px] 2xl:h-[19px] 3xl:w-[1.5vw] 3xl:h-[1.5vw] pr-[2px]"
-                                viewBox="0 0 21 16"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                                aria-label="Email icon"
-                            >
-                                <title>Email icon</title>
-                                <path
-                                    d="M18.1504 0H2.15039C1.05039 0 0.160391 0.9 0.160391 2L0.150391 14C0.150391 15.1 1.05039 16 2.15039 16H18.1504C19.2504 16 20.1504 15.1 20.1504 14V2C20.1504 0.9 19.2504 0 18.1504 0ZM18.1504 14H2.15039V4L10.1504 9L18.1504 4V14ZM10.1504 7L2.15039 2H18.1504L10.1504 7Z"
-                                    fill="white"
-                                />
-                            </svg>
+        <div className="interactive-map relative overflow-visible ml-5 pr-4 md:ml-[34px] lg:ml-[59px] xl:ml-[100px] 2xl:ml-[308px] h-[47vh] sm:h-[60vh] md:h-[500px] lg:h-[500px] xl:h-[600px] 2xl:h-[600px] 3xl:h-[40vw] w-auto border-t border-white">
+            {/* Parent Container: flex-col on mobile, flex-row on md and above */}
+            <div className="flex flex-col md:flex-row h-full">
+                {/* Left Section - "Let's Talk" and Address Display */}
+                <div className="w-full md:w-1/3 flex-shrink-0 overflow-visible z-10">
+                    {/* Left Section Content */}
+                    <div className="flex items-center">
+                        <div
+                            id="lets-talk"
+                            className="font-Helvetica font-bold text-[32px] md:text-[32px] lg:text-[55px] xl:text-[65px] 2xl:text-[4vw] leading-tight pt-2 md:pt-4"
+                        >
+                            Let’s talk
                         </div>
+                    </div>
+                    <div className="sm:pl-0 w-[200px] sm:w-[300px] xl:w-[350px] 2xl:w-[400px] 3xl:w-[25vw]">
+                        <img src={lineImg} alt="Divider Line" className="w-full h-auto" />
+                    </div>
+                    <a href="mailto:creative@kekstcnc.com">
+                        <div className="sm:pl-0 flex items-center gap-2 pt-6 sm:pt-3 lg:pt-6 xl:pt-10 2xl:pt-[57px] 3xl:pt-[3vw] hover:text-[#7a6b87]">
+                            <div className="flex items-center justify-center">
+                                <svg
+                                    className="w-[21px] h-[16px] 2xl:w-[25px] 2xl:h-[19px] 3xl:w-[1.5vw] 3xl:h-[1.5vw] pr-[2px]"
+                                    viewBox="0 0 21 16"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    aria-label="Email icon"
+                                >
+                                    <title>Email icon</title>
+                                    <path
+                                        d="M18.1504 0H2.15039C1.05039 0 0.160391 0.9 0.160391 2L0.150391 14C0.150391 15.1 1.05039 16 2.15039 16H18.1504C19.2504 16 20.1504 15.1 20.1504 14V2C20.1504 0.9 19.2504 0 18.1504 0ZM18.1504 14H2.15039V4L10.1504 9L18.1504 4V14ZM10.1504 7L2.15039 2H18.1504L10.1504 7Z"
+                                        fill="white"
+                                    />
+                                </svg>
+                            </div>
 
-                        <div className="flex items-center h-full">
-                            <div className="font-Helvetica font-bold sm:text-[17px] lg:text-[20px] xl:text-2xl 2xl:text-3xl 3xl:text-[1.2vw] pl-1 2xl:pl-[0.5vw]">
-                                creative@kekstcnc.com
+                            <div className="flex items-center h-full">
+                                <div className="font-Helvetica font-bold sm:text-[17px] lg:text-[20px] xl:text-2xl 2xl:text-3xl 3xl:text-[1.2vw] pl-1 2xl:pl-[0.5vw]">
+                                    creative@kekstcnc.com
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </a>
-                {/* Address Display */}
-                <div className="mt-16">
-                    {/* "Where we are:" text */}
-                    <div className="font-Helvetica font-extrabold text-[24px] md:text-[32px] lg:text-[32px] xl:text-[36px] 2xl:text-[38px] 3xl:text-[2.2vw] leading-tight text-white mt-3 lg:mt-6 mt-4 lg:mt-6 3xl:mt-[5vw]">
-                        Where we are:
-                    </div>
-
-                    {/* City Name with Icon */}
-                    <div className="flex items-center mb-2">
-                        {/* SVG Icon */}
-                        <div className="mr-2 lg:mr-4 xl:mr-6 mt-3 lg:mt-6 3xl:mt-[1.5vw]">
-                            <svg
-                                className="w-[20px] h-[20px] 2xl:w-[2vw] 2xl:h-[2vw]"
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="20"
-                                height="20"
-                                fill="white"
-                                viewBox="0 0 24 24"
-                            >
-                                <path d="M12 2c3.196 0 6 2.618 6 5.602 0 3.093-2.493 7.132-6 12.661-3.507-5.529-6-9.568-6-12.661 0-2.984 2.804-5.602 6-5.602m0-2c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z" />
-                            </svg>
+                    </a>
+                    {/* Address Display */}
+                    <div className="mt-8 md:mt-16">
+                        {/* "Where we are:" text */}
+                        <div className="font-Helvetica font-extrabold text-[24px] md:text-[32px] lg:text-[32px] xl:text-[36px] 2xl:text-[38px] 3xl:text-[2.2vw] leading-tight text-white mt-3 lg:mt-6">
+                            Where we are:
                         </div>
-                        {/* City Name */}
-                        <div className="font-Helvetica font-bold text-[22px] md:text-[30px] lg:text-[30px] xl:text-[34px] 2xl:text-[36px] 3xl:text-[2vw] leading-tight text-white mt-3 lg:mt-6 3xl:mt-[1.5vw]">
-                            {displayedCity?.city}
-                        </div>
-                    </div>
 
-                    {/* Address */}
-                    <div
-                        className="font-Helvetica sm:text-[17px] lg:text-[20px] xl:text-2xl 2xl:text-3xl 3xl:text-[1.1vw] font-normal whitespace-pre-line address-display leading-normal py-4 2xl:py-[1.5vw] 3xl:leading-[1.8vw]"
-                        style={{
-                            opacity: displayedCity ? '1' : '0',
-                            transition: 'opacity 0.2s ease',
-                        }}
-                    >
-                        {/* font-Helvetica font-bold sm:text-[17px] lg:text-[20px] xl:text-2xl 2xl:text-3xl 3xl:text-[1.2vw] pl-1 2xl:pl-[0.5vw] */}
-                        {displayedCity?.address}
+                        {/* City Name with Icon */}
+                        <div className="flex items-center mb-2">
+                            {/* SVG Icon */}
+                            <div className="mr-2 lg:mr-4 xl:mr-6 mt-3 lg:mt-6 3xl:mt-[1.5vw]">
+                                <svg
+                                    className="w-[20px] h-[20px] 2xl:w-[2vw] 2xl:h-[2vw]"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="20"
+                                    height="20"
+                                    fill="white"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path d="M12 2c3.196 0 6 2.618 6 5.602 0 3.093-2.493 7.132-6 12.661-3.507-5.529-6-9.568-6-12.661 0-2.984 2.804-5.602 6-5.602m0-2c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z" />
+                                </svg>
+                            </div>
+                            {/* City Name */}
+                            <div className="font-Helvetica font-bold text-[22px] md:text-[30px] lg:text-[30px] xl:text-[34px] 2xl:text-[36px] 3xl:text-[2vw] leading-tight text-white mt-3 lg:mt-6 3xl:mt-[1.5vw]">
+                                {displayedCity?.city}
+                            </div>
+                        </div>
+
+                        {/* Address */}
+                        <div
+                            className="h-[50px] md:h-auto font-Helvetica sm:text-[17px] lg:text-[20px] xl:text-2xl 2xl:text-3xl 3xl:text-[1.1vw] font-normal whitespace-pre-line address-display leading-normal py-4 2xl:py-[1.5vw] 3xl:leading-[1.8vw]"
+                            style={{
+                                opacity: displayedCity ? '1' : '0',
+                                transition: 'opacity 0.2s ease',
+                            }}
+                        >
+                            {displayedCity?.address}
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            {/* Right Section - Interactive Map */}
-            <div className="w-full md:w-2/3 flex-shrink-0 overflow-visible">
-                <div
-                    className="svg-container w-full mt-[100px] mt-8 md:mt-[120px]"
-                    style={{ position: 'relative' }}
-                >
-                    {/* Map Component */}
-                    <Map />
-                    {/* Overlay for Labels */}
+                {/* Right Section - Interactive Map */}
+                <div className="w-full md:w-2/3 flex-shrink-0 overflow-visible mt-8 md:mt-0">
                     <div
-                        className="overlay"
-                        style={{
-                            position: 'absolute',
-                            top: 0,
-                            left: 0,
-                            width: '100%',
-                            height: '100%',
-                            zIndex: 1,
-                            pointerEvents: 'none',
-                            overflow: 'hidden',
-                        }}
-                    ></div>
+                        className="svg-container w-full mt-[100px] mt-8 md:mt-[120px]"
+                        style={{ position: 'relative' }}
+                    >
+                        {/* Map Component */}
+                        <Map />
+                        {/* Overlay for Labels */}
+                        <div
+                            className="overlay"
+                            style={{
+                                position: 'absolute',
+                                top: 0,
+                                left: 0,
+                                width: '100%',
+                                height: '100%',
+                                zIndex: 1,
+                                pointerEvents: 'none',
+                                overflow: 'hidden',
+                            }}
+                        ></div>
+                    </div>
                 </div>
             </div>
         </div>
